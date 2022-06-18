91de75237cada3143217dbd1f5bd73a9777d7679
Sammy <segzyfaj1@gmail.com>
2022-06-18 19:15:32 +0100

    A new Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Hero from './components/Hero';
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Navbar />
+      <Home />
+      <Content />
+      <Footer />
      <h1 className="text-3xl font-bold underline">
-      Hello world!
+     
     </h1>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@
 
 const Navbar = () => {
   return (
-    <div bg-gray-100>
+    <div className="bg-gray-100 w-100 h-60">
         <nav>
             <div><h1>OrderNow</h1></div>
             <ul>
